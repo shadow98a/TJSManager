81f3384719f7f98e857b5ced464e09111b27def7
shadow98a <43942589+shadow98a@users.noreply.github.com>
2022-07-06 16:28:08 +0900

    initial commit

---
+++ TJSManager/src/main/java/com/tjs/tjsmanager/TjsManagerApplication.java
@@ -0,0 +1,13 @@
+package com.tjs.tjsmanager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TjsManagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TjsManagerApplication.class, args);
+	}
+
+}

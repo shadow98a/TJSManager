973a1bf0ef4e1f49428741586180e47d4878167a
sg0831 <dev.sg0831@gmail.com>
2022-07-25 16:02:38 +0900

    reupload ReqInWarehouse.

--- TJSManager/src/main/java/com/tjs/tjsmanager/controller/HrmController.java
+++ TJSManager/src/main/java/com/tjs/tjsmanager/controller/HrmController.java
@@ -1,12 +1,18 @@
 package com.tjs.tjsmanager.controller;
 
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
 
 import com.tjs.tjsmanager.domain.json.EmployeeLoginJson;
 import com.tjs.tjsmanager.service.HrmService;
 
+
+@RestController
 public class HrmController {
 	
 	@Autowired
@@ -14,7 +20,8 @@ public class HrmController {
 
 	// 로그인
 	@PostMapping("/employee/login")
-	public String employeeLogin(@RequestBody EmployeeLoginJson jsonData) {
-		return hrmService.employeeLogin(jsonData);
+	public String employeeLogin(@RequestBody EmployeeLoginJson jsonData, HttpServletRequest request) throws Exception {
+		HttpSession session = request.getSession();
+		return hrmService.employeeLogIn(jsonData, session);
 	}
 }
--- TJSManager/src/main/java/com/tjs/tjsmanager/service/HrmService.java
+++ TJSManager/src/main/java/com/tjs/tjsmanager/service/HrmService.java
@@ -1,26 +1,46 @@
 package com.tjs.tjsmanager.service;
 
+import javax.servlet.http.HttpSession;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 import com.tjs.tjsmanager.domain.hrm.Employee;
 import com.tjs.tjsmanager.domain.json.EmployeeLoginJson;
 import com.tjs.tjsmanager.repository.hrm.EmployeeRepository;
 
+
+
+@Service
 public class HrmService {
 
 	@Autowired
 	private EmployeeRepository employeeRepository;
 	
 	// 직원 로그인
-	public String employeeLogin(EmployeeLoginJson jsonData) {
+	public String employeeLogIn(EmployeeLoginJson jsonData, HttpSession session) {
 		if (employeeRepository.findById(jsonData.getEmpNum()) != null) {
 			Employee employee = employeeRepository.findById(jsonData.getEmpNum()).get();
 			
 			if (employee.getEmpPassword() == jsonData.getEmpPassword()) {
+				session.setAttribute("loginAccount", employee);
 				return "login success!";
 			}
 		}
 		return "login faild...";
+	}
+	
+	// 직원 로그아웃
+	public void employeeLogOut(HttpSession session) {
+		if (session.getAttribute("loginAccount") != null) {
+			session.removeAttribute("loginAccount");
+		}
+	}
+	
+	// 직원 로그인 여부 확인
+	public boolean isLogIn(HttpSession session) {
+		Employee employee = (Employee)session.getAttribute("loginAccount");
+		return (employee != null);
 	}
 	
 }
